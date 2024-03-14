2a85434c4876beba40d4c5b62160a665b87af5d5
allentsangdev <allentsang04@gmail.com>
2024-03-14 12:18:08 -0400

    update title

--- src/pages/cypress-test.tsx
+++ src/pages/cypress-test.tsx
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Coin Flip Challenge</title>
+        <title>Coin Flip Challenge - Cypress</title>
         <meta name="description" content="Ludex coin flip challenge!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
